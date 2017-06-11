54c6f7c3ef7f43106bc0db2bf733ed41b27cb811
Jan Jorissen <janjorissen@Jans-MacBook-Pro.local>
2017-06-11 22:42:46 +0200

    CORS support

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/EUR', function (req, res) {
     var request = require('request');
     request('https://ethereumprice.org/api/pairs/?p=eur', function (error, response, body) {
