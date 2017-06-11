4d5b27c258b4f429809cb34a93f89567177b5668
Jan Jorissen <janjorissen@Jans-MacBook-Pro.local>
2017-06-11 22:40:17 +0200

    change port

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.get('/USD_HIS', function (req, res) {
     });
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+    console.log('Example app listening on port 80!')
 })
