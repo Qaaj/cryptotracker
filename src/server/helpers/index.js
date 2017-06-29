b5ac869aace5829f2c1f6dc2c05c08753a888a1d
Jan Jorissen <grasmaaier45@gmail.com>
2017-06-29 17:25:50 +0200

    Bug where polling reset the Redis pair

--- src/server/helpers/index.js
+++ src/server/helpers/index.js
@@ -1,10 +1,14 @@
 const request = require('request-promise');
-const REDIS_TIMEOUT = 30;
+const REDIS_TIMEOUT = 20;
 
 module.exports = {
     getPair:  async(client, pair,endpoint) => {
-        let value = await client.getAsync(pair) || await request(endpoint.url);
-        client.set(pair, value, 'EX', REDIS_TIMEOUT);
+        let value = await client.getAsync(pair);
+
+        if(!value) {
+            value = await request(endpoint.url);
+            client.set(pair, value, 'EX', REDIS_TIMEOUT);
+        }
         return endpoint.parser(value);
     },
     isValid: (urls,pair) => {
