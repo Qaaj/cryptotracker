def1f1c1fa1d03e773db2015fb00bb616a5bbc4c
Jan Jorissen <grasmaaier45@gmail.com>
2017-07-05 20:23:13 +0200

    Downgrade to node 7 for Clustering + SocketIO

--- src/server/index.js
+++ src/server/index.js
@@ -1,44 +1,107 @@
 require('babel-register');
 
-const os = require('os');
-const cluster = require('cluster');
-const redis = require('redis');
 const bluebird = require('bluebird');
+const redis = require('redis');
 const adapter = require('socket.io-redis');
-var sticky = require('sticky-session');
-
-
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
 const redis_url = process.env.REDIS_URL || 'redis://localhost:6379';
-
 const client = redis.createClient(redis_url);
-// const cpus = os.cpus().length;
 
+const cluster = require('cluster'),
+    net = require('net'),
+    sio = require('socket.io')
 
-
-// var server = require('http').createServer(function(req, res) {
-//     res.end('worker: ' + cluster.worker.id);
-// });
-//
-// if (!sticky.listen(server, 3000)) {
-//     server.once('listening', function() {
-//         console.log('server started on 3000 port');
-//     });
-// } else {
-//     const redis_io = adapter(redis_url);
-//     require('./worker')(client,redis_io)
-// }
-
+const port = process.env.PORT || 3001;
+const num_processes = require('os').cpus().length;
 
 if (cluster.isMaster) {
-    for (let i = 0; i < (process.env.NODE_ENV === 'production' ? 1 : 1); i++) {
-        cluster.fork();
+    // This stores our workers. We need to keep them to be able to reference
+    // them based on source IP address. It's also useful for auto-restart,
+    // for example.
+    var workers = [];
+
+    // Helper function for spawning worker at index 'i'.
+    var spawn = function(i) {
+        workers[i] = cluster.fork();
+
+        // Optional: Restart worker on exit
+        workers[i].on('exit', function(code, signal) {
+            console.log('respawning worker', i);
+            spawn(i);
+        });
+    };
+
+    // Spawn workers.
+    for (var i = 0; i < num_processes; i++) {
+        spawn(i);
     }
+
+    // Helper function for getting a worker index based on IP address.
+    // This is a hot path so it should be really fast. The way it works
+    // is by converting the IP address to a number by removing non numeric
+    // characters, then compressing it to the number of slots we have.
+    //
+    // Compared against "real" hashing (from the sticky-session code) and
+    // "real" IP number conversion, this function is on par in terms of
+    // worker index distribution only much faster.
+
+    var worker_index = function(ip, len) {
+        var s = '';
+        for (var i = 0, _len = ip.length; i < _len; i++) {
+            if (!isNaN(ip[i])) {
+                s += ip[i];
+            }
+        }
+
+        return Number(s) % len;
+    };
+
+    // Create the outside facing server listening on our port.
+    var server = net.createServer({ pauseOnConnect: true }, function(connection) {
+        // We received a connection and need to pass it to the appropriate
+        // worker. Get the worker for this connection's source IP and pass
+        // it the connection.
+        var worker = workers[worker_index(connection.remoteAddress, num_processes)];
+        worker.send('sticky-session:connection', connection);
+    }).listen(port);
 } else {
-    const redis_io = adapter(redis_url);
-    require('./worker')(client,redis_io)
+    // Note we don't use a port here because the master listens on it for us.
+    var app = require('./worker')(client)
+
+    var server = app.listen(0, 'localhost');
+
+    const attachIO = (server) =>{
+
+        const redis_io = adapter(redis_url);
+        const sio = require('socket.io');
+        const io = sio.listen(server);
+
+        io.adapter(redis_io);
+
+        setInterval(() => io.emit('time', new Date().toTimeString()), 5000);
+
+        io.on('connection', socket => {
+            console.log('connection', socket.id);
+        });
+
+    }
+
+    attachIO(server);
+
+    // Listen to messages sent from the master. Ignore everything else.
+    process.on('message', function(message, connection) {
+        if (message !== 'sticky-session:connection') {
+            return;
+        }
+
+        // Emulate a connection event on the server by emitting the
+        // event with the connection the master sent us.
+        server.emit('connection', connection);
+
+        connection.resume();
+    });
 }
 
 client.on("error", console.error);
--- src/server/worker.js
+++ src/server/worker.js
@@ -2,10 +2,8 @@ const koa = require('koa');
 const app = new koa();
 const router = require('koa-router')();
 const serve = require('koa-static');
-const redis_url = process.env.REDIS_URL || 'redis://localhost:6379';
-const PORT = process.env.PORT || 3001;
 
-module.exports = (client,redis_io) => {
+module.exports = (client) => {
 
     const {CurrencyPairs, SSR} = require('./routes')(client);
 
@@ -13,29 +11,10 @@ module.exports = (client,redis_io) => {
     router.get('/api/:coin/:currency', CurrencyPairs);
     router.get('/', SSR);
 
-    router.get('/socket.io'),  async (ctx) => {
-        console.log(ctx);
-    }
-
     app
     .use(serve('./build/static/'))
     .use(router.routes())
     .use(router.allowedMethods());
 
-    const server = app.listen(PORT, () => console.log("Listening on port " + PORT));
-
-    const sio = require('socket.io');
-    const io = sio(server);
-
-    io.adapter(redis_io);
-
-    setInterval(() => io.emit('time', new Date().toTimeString()), 5000);
-
-    io.on('connection', socket => {
-        console.log('connection', socket.id);
-    });
-
-    return server;
-
-
+    return app;
 }
